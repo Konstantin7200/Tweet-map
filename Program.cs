8a308bd2e315dac71b4068fdc4f9464ae382f808
Nankevich Kirill <kirillnankevic@gmail.com>
2026-02-20 21:13:59 +0300

    State Parser,whitch get json and return States

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tweet_map
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
