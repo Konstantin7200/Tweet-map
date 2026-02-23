e23fe80fb898dee9cfbec158edbe14f9cf5fc4a8
Konstantin Sl <kostatop2@gmail.com>
2026-02-23 14:21:59 +0300

    Added dots display

--- TweetMapFrontend.cs
+++ TweetMapFrontend.cs
@@ -1,99 +1,126 @@
 ﻿using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.Reflection;
 using System.Text.Json;
 namespace program
 {
-    public class StateBoundary
-    {
-        public string StateCode { get; set; }
-        public List<List<PointF>> Polygons { get; set; }
-        public StateBoundary(string stateCode, List<List<PointF>> polygons)
-        {
-            StateCode = stateCode;
-            Polygons = polygons;
-        }
-    }
     class MapDrawer
-    {
-        static public void drawMap(List<StateBoundary> states)
         {
-            const int HEIGHT = 2500, WIDTH = 3500;
-            using (Bitmap bitmap = new Bitmap(WIDTH, HEIGHT))
+            private const int HEIGHT = 2500, WIDTH = 3500,DOT_RADIUS = 15;
+            static public void drawMap(List<StateBoundary> states,List<Tweet> tweets)
             {
-                using (Graphics graphics = Graphics.FromImage(bitmap))
+                using (Bitmap bitmap = new Bitmap(WIDTH, HEIGHT))
                 {
-                    graphics.Clear(Color.White);
-                    foreach (StateBoundary state in states)
+                    using (Graphics graphics = Graphics.FromImage(bitmap))
                     {
-                        Color color = getColor(Random.Shared.Next(-100, 100) * 1.0f / 100);
-                        foreach (List<PointF> polygon in state.Polygons)
+                        graphics.Clear(Color.White);
+                        foreach (StateBoundary state in states)
                         {
-                            correctPolygon(polygon, HEIGHT, WIDTH);
-                            drawPolygon(polygon, color,graphics);
+                            Color color = getColor(Random.Shared.Next(-100, 100) * 1.0f / 100);
+                            foreach (List<PointF> polygon in state.Polygons)
+                            {
+                                drawPolygon(correctPolygon(polygon), color, graphics);
+                            }
+
                         }
-                     
+                        drawDots(tweets,graphics);
+                        foreach (StateBoundary state in states)
+                        {
+                            drawStatesName(state.Polygons, state.StateCode, graphics);
+                        }
                     }
-                    foreach (StateBoundary state in states)
-                    {
-                        drawStatesName(state.Polygons[0], state.StateCode, graphics);
-                    }
+                    bitmap.Save("resultingMap.png", System.Drawing.Imaging.ImageFormat.Png);
                 }
-                bitmap.Save("resultingMap.png", System.Drawing.Imaging.ImageFormat.Png);
             }
-        }
-        static public void correctPolygon(List<PointF> polygon,int HEIGHT,int WIDTH)
-        {
-            for (int i = 0; i < polygon.Count; i++)
+            static private void drawDots(List<Tweet> tweets,Graphics graphics)
             {
-                polygon[i] = new PointF((polygon[i].X + 180) * 30, HEIGHT - (polygon[i].Y) * 30);
-            }
-        }
-        static public void drawStatesName(List<PointF> points,string name,Graphics graphics)
-        {
-            using (Font font = new Font("serif", 16, FontStyle.Bold))
-            {
-                using (Brush brush = new SolidBrush(Color.Black))
+                foreach(Tweet tweet in tweets)
                 {
-                    if(name!="FL")
+                    PointF point = correctPoint(new PointF((float)tweet.Latitude,(float)tweet.Longitude));
+                    Color color = getColor((float)tweet.SentimentScore);
+                    using (Brush brush = new SolidBrush(color))
                     {
-                        graphics.DrawString(name, font, brush, new PointF(((points.Max(p => p.X) + points.Min(p => p.X))) / 2, (points.Max(p => p.Y) + points.Min(p => p.Y)) / 2));
-                        return;
+                        graphics.FillEllipse(brush, point.X, point.Y, DOT_RADIUS, DOT_RADIUS);
                     }
-                    float x = 0, y = 0;
-                    foreach (PointF point in points)
+                    using (Pen pen = new Pen(Color.Black, 2))
                     {
-                        x += point.X;
-                        y += point.Y;
+                        graphics.DrawEllipse(pen,point.X,point.Y,DOT_RADIUS,DOT_RADIUS);
                     }
-                    x = x / points.Count;
-                    y = y / points.Count;
-                    graphics.DrawString(name, font, brush, new PointF(x, y));
                 }
             }
-        }
-        static public void drawPolygon(List<PointF> points, Color color,Graphics graphics)
-        {
-            using (Brush brush = new SolidBrush(color))
+            static private List<PointF> correctPolygon(List<PointF> polygon)
             {
-                graphics.FillPolygon(brush, points.ToArray());
+                List<PointF> correctedPolygon = new();
+                for (int i = 0; i < polygon.Count; i++)
+                {
+                    correctedPolygon.Add(correctPoint(polygon[i]));
+                }
+                return correctedPolygon;
             }
-            using (Pen pen = new Pen(Color.Black, 2))
+            static private PointF correctPoint(PointF point)
             {
-                graphics.DrawPolygon(pen, points.ToArray());
+                return new PointF((point.X + 180) * 30, HEIGHT - (point.Y) * 30);
             }
-        }
-        static private Color getColor(float? value)
-        {
-            int defaultColor = 128;
-            int colorMultiplier = 128;
-            if (value == null)
-                return Color.FromArgb(255, 192, 192, 192);
-            int red = Math.Max(Math.Min((int)(defaultColor + value * colorMultiplier * 2), 255),0);
-            int green = Math.Max(Math.Min((int)(defaultColor + Math.Abs((float)value) * colorMultiplier), 255),0);
-            int blue = Math.Max(Math.Min((int)(defaultColor - value * colorMultiplier * 1.5), 255),0);
-            Console.WriteLine(red+"|"+ green+"|"+ blue);
-            return Color.FromArgb(255,red,green,blue);
+            static private float calculateArea(List<PointF> polygon)
+            {
+                return (polygon.Max(p => p.X) - polygon.Min(p => p.X)) * (polygon.Max(p => p.Y) - polygon.Min(p => p.Y));
+            }
+            static private void drawStatesName(List<List<PointF>> polygons, string name, Graphics graphics)
+            {
+                List<PointF> points = correctPolygon(polygons[0]);
+                float maxArea = calculateArea(points);
+                for (int i=1;i<polygons.Count;i++)
+                {
+                    float newArea = calculateArea(polygons[i]);
+                    if(newArea>maxArea)
+                    {
+                        maxArea = newArea;
+                        points = correctPolygon(polygons[i]);
+                    }
+                }
+                using (Font font = new Font("serif", 16, FontStyle.Bold))
+                {
+                    using (Brush brush = new SolidBrush(Color.Black))
+                    {
+                        if (name != "FL")
+                        {
+                            graphics.DrawString(name, font, brush, new PointF(((points.Max(p => p.X) + points.Min(p => p.X))) / 2, (points.Max(p => p.Y) + points.Min(p => p.Y)) / 2));
+                            return;
+                        }
+                        float x = 0, y = 0;
+                        foreach (PointF point in points)
+                        {
+                            x += point.X;
+                            y += point.Y;
+                        }
+                        x = x / points.Count;
+                        y = y / points.Count;
+                        graphics.DrawString(name, font, brush, new PointF(x, y));
+                    }
+                }
+            }
+            static private void drawPolygon(List<PointF> points, Color color, Graphics graphics)
+            {
+                using (Brush brush = new SolidBrush(color))
+                {
+                    graphics.FillPolygon(brush, points.ToArray());
+                }
+                using (Pen pen = new Pen(Color.Black, 2))
+                {
+                    graphics.DrawPolygon(pen, points.ToArray());
+                }
+            }
+            static private Color getColor(float? value)
+            {
+                int defaultColor = 128;
+                int colorMultiplier = 128;
+                if (value == null)
+                    return Color.FromArgb(255, 192, 192, 192);
+                int red = Math.Max(Math.Min((int)(defaultColor + value * colorMultiplier * 2), 255), 0);
+                int green = Math.Max(Math.Min((int)(defaultColor + Math.Abs((float)value) * colorMultiplier), 255), 0);
+                int blue = Math.Max(Math.Min((int)(defaultColor - value * colorMultiplier * 1.5), 255), 0);
+                return Color.FromArgb(255, red, green, blue);
+            }
         }
     }
-}
