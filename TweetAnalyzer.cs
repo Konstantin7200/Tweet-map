9f50b9041b916b8551c8c2f6a0d02a987f32ceff
Andrei Astapchenia <andrey.ostapchenya@gmail.com>
2026-02-26 13:41:26 +0300

    Update groupbystate

--- TweetAnalyzer.cs
+++ TweetAnalyzer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
@@ -14,28 +14,38 @@ namespace program
         public static List<StateSentiment> AnalyzeTweets(List<Tweet> tweets, List<StateBoundary> stateBoundaries, Dictionary<string, float> sentiments)
         {
             AnalyzeTweetSentiments(tweets, sentiments);
-            return GroupByState(tweets, stateBoundaries);
+            return CreateStateSentiments(tweets, stateBoundaries);
         }
 
-        private static void GroupTweets(List<Tweet> tweets, List<StateBoundary> stateBoundaries)
+        private static void GroupTweetsByState(List<Tweet> tweets, List<StateBoundary> stateBoundaries)
         {
-            foreach (StateBoundary stateBoundary in stateBoundaries)
-            {
-                foreach (List<PointF> polygon in stateBoundary.Polygons)
-                {
-                    foreach (Tweet tweet in tweets)
-                    {
-                        using (GraphicsPath path = new GraphicsPath())
-                        {
-                            path.AddPolygon(polygon.ToArray());
-                            if (path.IsVisible(new PointF((float)tweet.Longitude, (float)tweet.Latitude)))
-                            {
-                                tweet.StateCode = stateBoundary.StateCode;
-                            }
-                        }
+            var statePaths = new Dictionary<string, GraphicsPath>();
 
+            foreach (var state in stateBoundaries)
+            {
+                var path = new GraphicsPath();
+                foreach (var polygon in state.Polygons)
+                {
+                    path.AddPolygon(polygon.ToArray());
+                }
+                statePaths[state.StateCode] = path;
+            }
+            foreach (var tweet in tweets)
+            {
+                var point = new PointF((float)tweet.Longitude, (float)tweet.Latitude);
+
+                foreach (var state in statePaths)
+                {
+                    if (state.Value.IsVisible(point))
+                    {
+                        tweet.StateCode = state.Key;
+                        break;
                     }
                 }
+            }
+            foreach (var path in statePaths.Values)
+            {
+                path.Dispose();
             }
         }
         private static void AnalyzeTweetSentiments(List<Tweet> tweets, Dictionary<string, float> sentiments)
@@ -70,18 +80,18 @@ namespace program
             }
         }
 
-        private static List<StateSentiment> GroupByState(List<Tweet> tweets, List<StateBoundary> statesBoundaries)
+        private static List<StateSentiment> CreateStateSentiments(List<Tweet> tweets, List<StateBoundary> statesBoundaries)
         {
-            GroupTweets(tweets, statesBoundaries);
+            GroupTweetsByState(tweets, statesBoundaries);
             var validTweets = tweets.Where(tweet => !string.IsNullOrEmpty(tweet.StateCode) && tweet.SentimentScore.HasValue).ToList();
             var groupTweets = validTweets.GroupBy(tweet => tweet.StateCode);
             var result = new List<StateSentiment>();
 
 
-            foreach(StateBoundary stateBoundary in statesBoundaries)
+            foreach (StateBoundary stateBoundary in statesBoundaries)
             {
                 var tweetsInState = groupTweets.FirstOrDefault(tweet => tweet.Key == stateBoundary.StateCode);
-                float? averageValue =tweetsInState==null?null:tweetsInState.Average(tweet => tweet.SentimentScore.Value);
+                float? averageValue = tweetsInState == null ? null : tweetsInState.Average(tweet => tweet.SentimentScore.Value);
                 StateSentiment stateSentiment = new StateSentiment(stateBoundary.StateCode, stateBoundary.Polygons)
                 {
                     AverageSentiment = averageValue
